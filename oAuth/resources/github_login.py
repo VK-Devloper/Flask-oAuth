e162819cb72466aa01b13c277bc68c6cde57642c
VK <mac@MACs-MacBook-Pro.local>
2020-09-15 21:24:14 +0530

    Initial commit

---
+++ oAuth/resources/github_login.py
@@ -0,0 +1,42 @@
+from flask import g, request, url_for
+from flask_restful import Resource
+from flask_jwt_extended import create_access_token, create_refresh_token
+
+from oa import github
+
+from models.user import UserModel
+
+
+class GithubLogin(Resource):
+    @classmethod
+    def get(cls):
+        return github.authorize(callback='http://localhost:5000/login/github/authorized')
+
+
+class GithubAuthorize(Resource):
+    @classmethod
+    def get(cls):
+        res = github.authorized_response()
+
+        if res is None or res.get('access_token') is None:
+            error_message = {
+                'error': request.args['error'],
+                'error_description': request.args['error_description']
+            }
+            return error_message
+
+        g.access_token = res['access_token']
+        github_user = github.get('user')
+        github_username = github_user.data['login']
+
+        user = UserModel.find_by_username(github_username)
+
+        if not user:
+            user = UserModel(username=github_username, password=None)
+            user.save_to_db()
+
+        access_token = create_access_token(identity=user.id, fresh=True)
+        refresh_token = create_refresh_token(identity=user.id)
+
+        return {'access_token': access_token, 'refresh_token': refresh_token}, 200
+
